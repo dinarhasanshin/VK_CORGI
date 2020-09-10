b94d45a116a8fe50517efee84602c41247795419
_____Dinar______ _____Dinar______ <hudazi0158@gmail.com>
2020-09-10 15:07:57 +0400

    Написан главный класс команды

---
+++ VK_CORGI/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VK_CORGI.Infrastructure.Commands.Base;
+
+namespace VK_CORGI.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));//Проверка на передачу в Execute данных.
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => Execute(parameter);
+    }
+}
